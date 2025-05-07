117321c5a0f2d869348c440dfa8a4169ed34381a
wkdghdus <--global>
2025-05-07 18:22:48 -0400

    feat: version 1 of the tokenizer, cannot handle unknown words

---
+++ data_preparation_and_sampling/tokenizer_v1.py
@@ -0,0 +1,24 @@
+### tokenizer classes that implements the demonstrations of token_to_token_id.py 
+### This tokenizer cannot handle unknown words that is not contatined in the vocab parameter.
+
+import re
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+    
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+                                
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+        
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
